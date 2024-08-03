9a5be0c62509cbe2438f2b07a4ac4e5cb14b9368
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-08-03 09:19:37 -0500

    added more comments

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
+// Load environment variables from a .env file into process.env
 require('dotenv').config();
 
+
+// Import Sequelize library for interacting with the database
 const Sequelize = require('sequelize');
 
+
+// Create a new Sequelize instance using environment variables
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
+  ? // If DB_URL is defined in the environment, use it to configure Sequelize
+    new Sequelize(process.env.DB_URL)
+  : // Otherwise, use individual environment variables for database configuration
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost', 
+      dialect: 'postgres', 
       dialectOptions: {
-        decimalNumbers: true,
+        decimalNumbers: true, // Ensure that numbers are parsed as decimals
       },
     });
 
+
+// Export the Sequelize instance for use in other parts of the application
 module.exports = sequelize;
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 
+-- Drop the database named 'ecommerce_db' if it already exists
 DROP DATABASE IF EXISTS ecommerce_db;
 
+-- Create a new database named 'ecommerce_db'
 CREATE DATABASE ecommerce_db;
-
